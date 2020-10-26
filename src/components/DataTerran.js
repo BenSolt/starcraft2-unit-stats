f9f33dc8cf5659bd559b5f3993b24019b5e9dd69
benjamin-solt <bensolt@gmail.com>
2020-10-25 20:04:21 -0400

    terran data complete

--- src/components/DataTerran.js
+++ src/components/DataTerran.js
@@ -26,8 +26,7 @@ const Terran = [
         supply: 1,
         minerals: 50,
         gas: 0,
-        health: 20,
-        shield: 20,
+        health: 45,
         groundAttack: 5,
         airAttack: 0,
         bonus: 0
@@ -36,11 +35,10 @@ const Terran = [
     {
         name: "MULE",
         image: Img_Zealot,
-        supply: 2,
-        minerals: 100,
+        supply: 0,
+        minerals: 0,
         gas: 0,
-        health: 100,
-        shield: 50,
+        health: 60,
         groundAttack: 8,
         airAttack: 0,
         bonus: 0
@@ -49,11 +47,10 @@ const Terran = [
     {
         name: "MARINE",
         image: Img_Sentry,
-        supply: 2,
+        supply: 1,
         minerals: 50,
-        gas: 100,
-        health: 40,
-        shield: 40,
+        gas: 0,
+        health: 45,
         groundAttack: 6,
         airAttack: 6,
         bonus: 0
@@ -63,12 +60,11 @@ const Terran = [
         name: "Marauder",
         image: Img_Stalker,
         supply: 2,
-        minerals: 125,
-        gas: 50,
-        health: 80,
-        shield: 80,
-        groundAttack: 13,
-        airAttack: 13,
+        minerals: 100,
+        gas: 25,
+        health: 125,
+        groundAttack: 5,
+        airAttack: 0,
         bonus: '+' + 5 + ' Armored'
     },
 
@@ -76,195 +72,180 @@ const Terran = [
         name: "Reaper",
         image: Img_Adept,
         supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
+        minerals: 50,
+        gas: 50,
+        health: 60,
+        groundAttack: 4,
+        airAttack: 0,
         bonus: 1
     },
 
     {
         name: "Ghost",
         image: Img_HighTemplar,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 2,
+        minerals: 150,
+        gas: 125,
+        health: 100,
+        groundAttack: 10,
+        airAttack: 10,
+        bonus: '+'+ 10 +'Light'
     },
 
     {
         name: "Hellion",
         image: Img_DarkTemplar,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 2,
+        minerals: 100,
+        gas: 0,
+        health: 90,
+        groundAttack: 8,
+        airAttack: 0,
+        bonus: '+'+ 5 +'Light'
     },
 
     {
         name: "Hellbat",
         image: Img_Archon,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
+        supply: 2,
+        minerals: 100,
+        gas: 0,
+        health: 135,
+        groundAttack: 18,
         airAttack: 1,
-        bonus: 1
+        bonus: '+'+ 12 +'Light'
     },
 
     {
         name: "Widow Mine",
         image: Img_Observer,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 2,
+        minerals: 75,
+        gas: 25,
+        health: 90,
+        groundAttack: 125,
+        airAttack: 0,
+        bonus: '+'+ 35+'Shield'
     },
 
     {
         name: "Siege Tank",
         image: Img_Immortal,
-        supply: 1,
+        supply: 3,
         minerals: 1,
         gas: 1,
         health: 1,
-        shield: 1,
-        groundAttack: 1,
+        groundAttack: 15 +'Tank' + 40 +'Seige',
         airAttack: 1,
-        bonus: 1
+        bonus: '+'+'Tank:'+ 15 +'armored' + 'Siege:'+'+'+ 30 + 'armored'
     },
 
     {
         name: "Cyclone",
         image: Img_WarpPrism,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 3,
+        minerals: 150,
+        gas: 100,
+        health: 120,
+        groundAttack: 18,
+        airAttack: 18,
+        bonus: 0
     },
 
     {
         name: "Thor",
         image: Img_Colossus,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 6,
+        minerals: 300,
+        gas: 200,
+        health: 400,
+        groundAttack: 'Explosive:'+30 + 'High Impact:'+30,
+        airAttack: 'Explosive:'+6 + 'High Impact:'+40,
+        bonus: 'Explosive:'+ '+'+6+'Light' +'High Impact:'+'+'+15+'Massive'
     },
 
     {
         name: "Viking",
         image: Img_Disruptor,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 2,
+        minerals: 150,
+        gas: 75,
+        health: 125,
+        groundAttack: 12,
+        airAttack: 10,
+        bonus: 'Air:'+'+'+ 4+'Armored' +'Ground:' + '+'+8+'Massive'
     },
 
     {
         name: "Medivac",
         image: Img_Phoenix,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 2,
+        minerals: 100,
+        gas: 100,
+        health: 150,
+        groundAttack: 0,
+        airAttack: 0,
+        bonus: 0
     },
 
     {
         name: "Liberator",
         image: Img_Oracle,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 3,
+        minerals: 150,
+        gas: 150,
+        health: 180,
+        groundAttack: 75,
+        airAttack: 5,
+        bonus: 0
     },
 
     {
         name: "Banshee",
         image: Img_VoidRay,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 3,
+        minerals: 150,
+        gas: 100,
+        health: 140,
+        groundAttack: 12,
+        airAttack: 0,
+        bonus: 0
     },
 
     {
         name: "Raven",
         image: Img_Tempest,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 2,
+        minerals: 100,
+        gas: 200,
+        health: 140,
+        groundAttack: 0,
+        airAttack: 0,
+        bonus: 0
     },
 
     {
         name: "Battlecruiser",
         image: Img_Carrier,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 6,
+        minerals: 400,
+        gas: 300,
+        health: 550,
+        groundAttack: 8,
+        airAttack: 6,
+        bonus: 0
     },
 
     {
         name: "Missle Turret",
         image: Img_Mothership,
-        supply: 1,
-        minerals: 1,
-        gas: 1,
-        health: 1,
-        shield: 1,
-        groundAttack: 1,
-        airAttack: 1,
-        bonus: 1
+        supply: 0,
+        minerals: 100,
+        gas: 0,
+        health: 250,
+        groundAttack: 0,
+        airAttack: 12 +'x' +2,
+        bonus: 0
     }
 ]
 
